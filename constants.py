4c05e6285487718bbce1aa35e38e712c3e61a005
AlexeyAndreev86 <aleksei.andreyev@gmail.com>
2023-01-14 19:02:03 +0300

    версия без БД

--- constants.py
+++
@@ -1,2 +0,0 @@
-# ID рабочей папки на google-disk
-GOOGLE_DRIVE_FOLDER_ID = '1G66jpbXYwpxPKdeLmWY10ndnJPzup4TX'
--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,13 +1,9 @@
 from aiogram import types, Dispatcher
 from keyboard.keyboard import get_keyboard, get_one_button
 from bot import bot
-from db_postgres import DB
-# from google_drive_api import GoogleDiskAPI
-
+from random import choice
 
 dp = Dispatcher(bot)
-db = DB()
-# disk = GoogleDiskAPI()
 
 
 @dp.message_handler()
@@ -22,34 +18,31 @@ async def send_cat(call: types.CallbackQuery):
     chat = call.from_user.id
     keyboard = get_keyboard()
 
-    # with open('file_id.txt', 'r') as file:
-    #     photos = file.readlines()
-    # images = [path.strip() for path in photos]
-    # file_for_send = choice(images)
-
-    db.cursor.execute('SELECT file_id FROM photos ORDER BY RANDOM() LIMIT 1;')
-    file_for_send = db.cursor.fetchone()[0]
-
-    await bot.send_photo(chat_id=chat, photo=file_for_send)
-    await bot.send_message(chat_id=chat, text='Жми кнопку или кидай фото в чат', reply_markup=keyboard)
+    with open('file_id.txt', 'r') as file:
+        photos = file.readlines()
+    images = [path.strip() for path in photos]
+    if len(images) > 0:
+        file_for_send = choice(images)
+        await bot.send_photo(chat_id=chat, photo=file_for_send)
+        await bot.send_message(chat_id=chat, text='Жми кнопку или кидай фото в чат', reply_markup=keyboard)
+    else:
+        await bot.send_message(chat_id=chat, text='У нас пока нет котегов :(')
 
     @dp.callback_query_handler(text='how_many')
     async def cat_counter(call: types.CallbackQuery):
         chat = call.from_user.id
         keyboard = get_one_button()
 
-        db.cursor.execute('SELECT COUNT(file_id) FROM photos;')
-        count = db.cursor.fetchone()[0]
+        with open('file_id.txt', 'r') as file:
+            photos = file.readlines()
+        count = len([path.strip() for path in photos])
 
         await bot.send_message(chat_id=chat, text=f'У нас {count} котегов', reply_markup=keyboard)
 
 
 @dp.message_handler(content_types=[types.ContentType.PHOTO])
 async def echo_document(message: types.Message):
-    """
-    Пишем ID фотки в базу и в файл :)
-    А хотелось писать в гугл диск.
-    """
+
     id = message.photo[-1].file_id
     chat = message.from_user.id
     keyboard = get_keyboard()
@@ -57,7 +50,5 @@ async def echo_document(message: types.Message):
     with open('file_id.txt', 'a') as f:
         f.write(id+'\n')
 
-    db.cursor.execute(f"INSERT INTO photos (file_id) VALUES ('{id}');")
-    db.connection.commit()
     await message.reply('Класс! Давай исчо!')
     await bot.send_message(chat_id=chat, text='Жми кнопку или кидай фото в чат', reply_markup=keyboard)
